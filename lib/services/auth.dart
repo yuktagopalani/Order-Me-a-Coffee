949ff7b0028f2f9fc68cd908bb1dc93cb60f29e4
yukta <iit2019052@iiita.ac.in>
2021-02-28 20:38:52 +0530

    Added Sigining out method

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,16 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
-
-import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:order_me_a_coffee/modules/user.dart';
 
 class AuthService{
-
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  MyUser _userFromFirebaseUser(User user)
+  {
+    return user != null ? MyUser(uid: user.uid): null;
+  }
+
+  Stream<MyUser> get user {
+    return _auth.authStateChanges().map(_userFromFirebaseUser);
+  }
 
   Future signInAnon() async {
     try{
       UserCredential result = await _auth.signInAnonymously();
       User user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     }
     catch(e)
     {
@@ -18,9 +26,17 @@ class AuthService{
         return null;
     }
   }
-
-
-
+  Future signingOut() async {
+    try{
+      return await _auth.signOut();
+    }
+    catch(e)
+    {
+      print(e);
+      return null;
+    }
+  }
 }
+
 
 
